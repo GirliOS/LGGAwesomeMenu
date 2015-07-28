efc40c1adfa985913d9e0bab750bef0c978d80a1
GirliOS <myladyios@gmail.com>
2015-07-28 14:28:09 +0800

    Initial Commit

---
+++ LGGAwesomeMenu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LGGAwesomeMenu
+//
+//  Created by baijf on 7/28/15.
+//  Copyright (c) 2015 GirliOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
